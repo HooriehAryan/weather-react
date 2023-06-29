b524e36a0c7105c9ec4a9533f4d8325dd110a120
H <ha8086@gmail.com>
2023-06-29 18:19:22 +0330

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `the weather in ${response.data.name} is ${response.data.main.temp} C`
+    );
+  }
+  let apiKey = "17ad6e67aa629189f73b053634668b20";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return <h2>Hello Weather</h2>;
+}
