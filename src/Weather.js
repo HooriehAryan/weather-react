9d436875f9ea2643a6f1e5c2f4e9461d5a1889cb
H <ha8086@gmail.com>
2023-06-30 11:02:09 +0330

    change weather

--- src/Weather.js
+++ src/Weather.js
@@ -11,5 +11,5 @@ export default function Weather(props) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
 
-  return <h2>Hello Weather</h2>;
+  return <h2>Hello Weather react</h2>;
 }
